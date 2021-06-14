a135f9ff5e1e0f1c253dd39859fdc79a97cf20a1
zayson <jymaeng95@gmail.com>
2021-06-14 22:57:51 +0900

    Singleton

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class XmlAppContext {
     @Test
     void xmlContext() {
