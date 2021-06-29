ff908d76db3d9d5fb3a875867107faed8db81c3e
zayson <jymaeng95@gmail.com>
2021-06-29 22:56:33 +0900

    component scan

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         //Configuration에 Component 어노테이션이 붙어있기 때문에 제외
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
