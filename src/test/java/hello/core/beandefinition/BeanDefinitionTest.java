96270a6c7a3152dc71fd4a0dc2bdc7c9be7b0117
zayson <jymaeng95@gmail.com>
2022-03-29 11:19:19 +0900

    [Feat] ADD Remind Spring Develop Day 4

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -33,8 +33,8 @@ public class BeanDefinitionTest {
             BeanDefinition beanDefinition = genericXmlApplicationContext.getBeanDefinition(beanDefinitionName);
 
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
-                System.out.println("beanDefinitionName = " + beanDefinitionName + "beanDefinition = "+ beanDefinition);
-
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "beanDefinition = " + beanDefinition);
+            }
         }
     }
 }
